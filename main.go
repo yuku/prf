2ce60f9cb2e55efd9d4206a7f5d63e6e851cc389
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-12-19 16:19:00 +0900

    Refactor a bit

--- main.go
+++ main.go
@@ -14,49 +14,52 @@ import (
   "golang.org/x/oauth2"
 )
 
-func main() {
+var regexps = []*regexp.Regexp{
+  regexp.MustCompile(`^git@[^:]+:(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
+  regexp.MustCompile(`^ssh://git@[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
+  regexp.MustCompile(`^https://[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
+}
+
+func parseURL(url string) (string, string) {
+  for _, r := range regexps {
+    m := r.FindStringSubmatch(url)
+    if len(m) > 0 {
+      return m[1], m[2]
+    }
+  }
+  log.Fatal("not github repo")
+  return "", ""
+}
+
+func getRemoteURL() (string) {
   out, err := exec.Command("git", "remote", "get-url", "origin").Output()
   if err != nil {
     log.Fatal(err)
   }
-  url := strings.TrimSpace(string(out))
-  var owner string
-  var repo string
-  r1 := regexp.MustCompile(`^git@[^:]+:(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`)
-  m1 := r1.FindStringSubmatch(url)
-  if len(m1) > 0 {
-    owner = m1[1]
-    repo = m1[2]
-  } else {
-    r2 := regexp.MustCompile(`^ssh://git@[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`)
-    m2 := r2.FindStringSubmatch(url)
-    if len(m2) > 0 {
-      owner = m2[1]
-      repo = m2[2]
-    } else {
-      r3 := regexp.MustCompile(`^https://[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`)
-      m3 := r3.FindStringSubmatch(url)
-      if len(m3) > 0 {
-        owner = m3[1]
-        repo = m3[2]
-      } else {
-        log.Fatal("not github repo")
-      }
-    }
-  }
+  return strings.TrimSpace(string(out))
+}
 
+func getAccessToken() (string) {
   usr, err := user.Current()
   if err != nil {
-    log.Fatal( err )
+    log.Fatal(err)
   }
-  m, _ := netrc.FindMachine(filepath.Join(usr.HomeDir, ".netrc"), "api.github.com")
+  m, err := netrc.FindMachine(filepath.Join(usr.HomeDir, ".netrc"), "api.github.com")
+  if err != nil {
+    log.Fatal(err)
+  }
+  return m.Password
+}
 
-  ts := oauth2.StaticTokenSource(
-    &oauth2.Token{AccessToken: m.Password},
-  )
+func main() {
+  accessToken := getAccessToken()
+
+  ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
   tc := oauth2.NewClient(oauth2.NoContext, ts)
   client := github.NewClient(tc)
 
+  url := getRemoteURL()
+  owner, repo := parseURL(url)
   pulls, _, err := client.PullRequests.List(owner, repo, nil)
   if err != nil {
     log.Fatal(err)
