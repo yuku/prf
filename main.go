dae8d98a58566e73f8f202ad83cfeed691e02088
Yuku TAKAHASHI <taka84u9@gmail.com>
2019-03-25 15:14:02 +0900

    Update code format

--- main.go
+++ main.go
@@ -1,73 +1,73 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "log"
-  "os/exec"
-  "os/user"
-  "regexp"
-  "strings"
-  "path/filepath"
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strings"
 
-  "github.com/bgentry/go-netrc/netrc"
-  "github.com/google/go-github/github"
-  "golang.org/x/oauth2"
+	"github.com/bgentry/go-netrc/netrc"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 var regexps = []*regexp.Regexp{
-  regexp.MustCompile(`^git@[^:]+:(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
-  regexp.MustCompile(`^ssh://git@[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
-  regexp.MustCompile(`^https://[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
+	regexp.MustCompile(`^git@[^:]+:(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
+	regexp.MustCompile(`^ssh://git@[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
+	regexp.MustCompile(`^https://[^/]+/(?P<owner>[^/]+)/(?P<repo>.+?)(?:\.git)?$`),
 }
 
 func parseURL(url string) (string, string) {
-  for _, r := range regexps {
-    m := r.FindStringSubmatch(url)
-    if len(m) > 0 {
-      return m[1], m[2]
-    }
-  }
-  log.Fatal("not github repo")
-  return "", ""
+	for _, r := range regexps {
+		m := r.FindStringSubmatch(url)
+		if len(m) > 0 {
+			return m[1], m[2]
+		}
+	}
+	log.Fatal("not github repo")
+	return "", ""
 }
 
-func getRemoteURL() (string) {
-  out, err := exec.Command("git", "remote", "get-url", "origin").Output()
-  if err != nil {
-    log.Fatal(err)
-  }
-  return strings.TrimSpace(string(out))
+func getRemoteURL() string {
+	out, err := exec.Command("git", "remote", "get-url", "origin").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(string(out))
 }
 
-func getAccessToken() (string) {
-  usr, err := user.Current()
-  if err != nil {
-    log.Fatal(err)
-  }
-  m, err := netrc.FindMachine(filepath.Join(usr.HomeDir, ".netrc"), "api.github.com")
-  if err != nil {
-    log.Fatal(err)
-  }
-  return m.Password
+func getAccessToken() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := netrc.FindMachine(filepath.Join(usr.HomeDir, ".netrc"), "api.github.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m.Password
 }
 
 func main() {
-  accessToken := getAccessToken()
+	accessToken := getAccessToken()
 
-  ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
-  tc := oauth2.NewClient(oauth2.NoContext, ts)
-  client := github.NewClient(tc)
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
 
-  ctx := context.TODO()
-  url := getRemoteURL()
-  owner, repo := parseURL(url)
-  pulls, _, err := client.PullRequests.List(ctx, owner, repo, nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  for _, p := range pulls {
-    b := *p.Head
-    fmt.Printf("#%v %v\t%v\n", *p.Number, *p.Title, *b.Ref)
-  }
+	ctx := context.TODO()
+	url := getRemoteURL()
+	owner, repo := parseURL(url)
+	pulls, _, err := client.PullRequests.List(ctx, owner, repo, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, p := range pulls {
+		b := *p.Head
+		fmt.Printf("#%v %v\t%v\n", *p.Number, *p.Title, *b.Ref)
+	}
 }
